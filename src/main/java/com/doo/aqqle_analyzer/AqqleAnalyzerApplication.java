0980334f62db477c0ab96d051562209b67b0e390
doo <doohyun.lee@homeplus.co.kr>
2024-01-19 20:11:49 +0900

    init

---
+++ src/main/java/com/doo/aqqle_analyzer/AqqleAnalyzerApplication.java
@@ -0,0 +1,13 @@
+//package com.doo.aqqle_analyzer;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class AqqleAnalyzerApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AqqleAnalyzerApplication.class, args);
+//	}
+//
+//}
